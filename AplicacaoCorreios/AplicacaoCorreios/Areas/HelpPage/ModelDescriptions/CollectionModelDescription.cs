93699cdbf995b1be94908a40d5fc7bcf6137b934
hbsis <hbsis@BLUE07>
2019-10-22 17:06:15 -0300

    sim.

---
+++ AplicacaoCorreios/AplicacaoCorreios/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AplicacaoCorreios.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AplicacaoCorreios/AplicacaoCorreios/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplicacaoCorreios.WebApiApplication" Language="C#" %>
