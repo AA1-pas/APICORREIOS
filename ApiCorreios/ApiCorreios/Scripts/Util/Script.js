e1c4178155c40f6cd0b81695206ea81b7bf825b4
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-10-25 11:16:10 -0300

    commit inclusao dos erros

--- ApiCorreios/ApiCorreios/Scripts/Util/Script.js
+++ ApiCorreios/ApiCorreios/Scripts/Util/Script.js
@@ -5,15 +5,15 @@ var erros = [
     },
     {
         erro: -1,
-        msg: "Código de serviço inválido"
+        msg: "Codigo de servico invalido"
     },
     {
         erro: -2,
-        msg: "CEP de origem inválido"
+        msg: "CEP de origem invalido"
     },
     {
         erro: -3,
-        msg: "CEP de destino inválido"
+        msg: "CEP de destino invalido"
     },
     {
         erro: -4,
@@ -21,64 +21,64 @@ var erros = [
     },
     {
         erro: -5,
-        msg: "O Valor Declarado não deve exceder R$ 10.000,00"
+        msg: "O Valor Declarado nao deve exceder R$ 10.000,00"
     },
     {
         erro: -6,
-        msg: "Serviço indisponível para o trecho informado"
+        msg: "Servico indisponivel para o trecho informado"
     },
     {
         erro: -7,
-        msg: "O Valor Declarado é obrigatório para este serviço"
+        msg: "O Valor Declarado e obrigatorio para este servico"
     },
     {
         erro: -8,
-        msg: "Este serviço não aceita Mão Própria"
+        msg: "Este servico nao aceita Mao Propria"
     },
     {
         erro: -9,
-        msg: "Este serviço não aceita Aviso de Recebimento"
+        msg: "Este servico nao aceita Aviso de Recebimento"
     },
     {
         erro: -10,
-        msg: "Precificação indisponível para o trecho informado"
+        msg: "Precificacao indisponivel para o trecho informado"
     },
     {
         erro: -11,
-        msg: "Para definição do preço deverão ser informados, também, o comprimento, a largura e altura do objeto em centímetros(cm)."
+        msg: "Para definicao do preco deverao ser informados, tambem, o comprimento, a largura e altura do objeto em centimetros(cm)."
     },
     {
         erro: -12,
-        msg: "Comprimento inválido."
+        msg: "Comprimento invalido."
     },
     {
         erro: -13,
-        msg: "Largura inválida."
+        msg: "Largura invalida."
     },
     {
         erro: -14,
-        msg: "Altura inválida."
+        msg: "Altura invalida."
     },
     {
         erro: -15,
-        msg: "O comprimento não pode ser maior que 105 cm."
+        msg: "O comprimento nao pode ser maior que 105 cm."
     },
     {
         erro: -16,
-        msg: "A largura não pode ser maior que 105 cm."
+        msg: "A largura nao pode ser maior que 105 cm."
     },
     {
         erro: -17,
-        msg: "A altura não pode ser maior que 105 cm."
+        msg: "A altura nao pode ser maior que 105 cm."
     },
     {
         erro: -18,
-        msg: "A altura não pode ser inferior a 2 cm."
+        msg: "A altura nao pode ser inferior a 2 cm."
     },
 
     {
         erro: -20,
-        msg: "A largura não pode ser inferior a 11 cm."
+        msg: "A largura nao pode ser inferior a 11 cm."
     },
     {
         erro: -21,
@@ -86,19 +86,19 @@ var erros = [
     },
     {
         erro: -22,
-        msg: "O comprimento não pode ser inferior a 16 cm."
+        msg: "O comprimento nao pode ser inferior a 16 cm."
     },
     {
         erro: -23,
-        msg: "A soma resultante do comprimento + largura + altura não deve superar a 200 cm."
+        msg: "A soma resultante do comprimento + largura + altura nao deve superar a 200 cm."
     },
     {
         erro: -24,
-        msg: "Comprimento inválido."
+        msg: "Comprimento invalido."
     },
     {
         erro: -25,
-        msg: "Diâmetro inválido"
+        msg: "Diametro invalido"
     },
     {
         erro: -26,
@@ -106,27 +106,27 @@ var erros = [
     },
     {
         erro: -27,
-        msg: "Informe o diâmetro."
+        msg: "Informe o diametro."
     },
     {
         erro: -28,
-        msg: "O comprimento não pode ser maior que 105 cm."
+        msg: "O comprimento nao pode ser maior que 105 cm."
     },
     {
         erro: -29,
-        msg: "O diâmetro não pode ser maior que 91 cm."
+        msg: "O diametro nao pode ser maior que 91 cm."
     },
     {
         erro: -30,
-        msg: "O comprimento não pode ser inferior a 18 cm."
+        msg: "O comprimento nao pode ser inferior a 18 cm."
     },
     {
         erro: -31,
-        msg: "O diâmetro não pode ser inferior a 5 cm."
+        msg: "O diametro nao pode ser inferior a 5 cm."
     },
     {
         erro: -32,
-        msg: "A soma resultante do comprimento + o dobro do diâmetro não deve superar a 200 cm."
+        msg: "A soma resultante do comprimento + o dobro do diametro nao deve superar a 200 cm."
     },
     {
         erro: -33,
@@ -134,7 +134,7 @@ var erros = [
     },
     {
         erro: -34,
-        msg: "Código Administrativo ou Senha inválidos."
+        msg: "Codigo Administrativo ou Senha invalidos."
     },
     {
         erro: -35,
@@ -142,15 +142,15 @@ var erros = [
     },
     {
         erro: -36,
-        msg: "Cliente não possui contrato vigente com os Correios."
+        msg: "Cliente nao possui contrato vigente com os Correios."
     },
     {
         erro: -37,
-        msg: "Cliente não possui serviço ativo em seu contrato."
+        msg: "Cliente nao possui servico ativo em seu contrato."
     },
     {
         erro: -38,
-        msg: "Serviço indisponível para este código administrativo."
+        msg: "Servico indisponivel para este codigo administrativo."
     },
     {
         erro: -39,
@@ -186,27 +186,27 @@ var erros = [
     },
     {
         erro: "006",
-        msg: "Localidade de origem não abrange o serviço informado"
+        msg: "Localidade de origem nao abrange o servico informado"
     },
     {
         erro: "007",
-        msg: "Localidade de destino não abrange o serviço informado"
+        msg: "Localidade de destino nao abrange o servico informado"
     },
     {
         erro: "008",
-        msg: "Serviço indisponível para o trecho informado"
+        msg: "Servico indisponivel para o trecho informado"
     },
     {
         erro: "009",
-        msg: "CEP inicial pertencente a Área de Risco."
+        msg: "CEP inicial pertencente a area de Risco."
     },
     {
         erro: "010",
-        msg: "CEP de destino está temporariamente sem entrega domiciliar.A entrega será efetuada na agência indicada no Aviso de Chegada que será entregue no endereço do destinatário 011 CEP de destino está sujeito a condições especiais de entrega pela ECT e será realizada com o acréscimo de até 7(sete) dias úteis ao prazo regular.."
+        msg: "CEP de destino esta temporariamente sem entrega domiciliar.A entrega sera efetuada na agência indicada no Aviso de Chegada que sera entregue no endereco do destinatario 011 CEP de destino esta sujeito a condicoes especiais de entrega pela ECT e sera realizada com o acrescimo de ate 7(sete) dias úteis ao prazo regular.."
     },
     {
         erro: 7,
-        msg: "Serviço indisponível, tente mais tarde"
+        msg: "Servico indisponivel, tente mais tarde"
     },
     {
         erro: 99,
@@ -229,7 +229,6 @@ $(document).ready(function () {
             $.each(erros, function (key, value) {
                 if (value.erro == data.cServico.Erro)
                     $('span[name="erro"').text(value.msg);
-            });
             
         });
 
@@ -237,5 +236,5 @@ $(document).ready(function () {
         
     });
 });
-
+});
 
